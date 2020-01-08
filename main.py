8ce17ffe157ec77ae1b8600331bb4c298a9d0e2b
Nam Vo <namvo@Nams-MacBook-Pro.local>
2020-01-08 14:39:19 -0800

    Lesson 5 Assignment

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-
 def get_fact():
 
     response = requests.get("http://unkno.com")
@@ -14,13 +13,30 @@ def get_fact():
     soup = BeautifulSoup(response.content, "html.parser")
     facts = soup.find_all("div", id="content")
 
+    print(f"facts = {facts}")
+
     return facts[0].getText()
 
+def get_piglatin(fact):
+
+    url = "https://hidden-journey-62459.herokuapp.com/piglatinize/"
+    data = {"input_text": fact}
+    response = requests.post(url, data, allow_redirects=False)
+
+    soup = BeautifulSoup(response.content, "html.parser")
+    redirect_link = soup.a.get('href')
+
+    return url.replace('/piglatinize/', redirect_link)
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    fact = get_fact().strip()
 
+    print(f"fact = {fact}")
+
+    result = get_piglatin(fact)
+
+    return result
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
